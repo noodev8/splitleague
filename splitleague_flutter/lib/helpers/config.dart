1d8df5446f33d24372da331a5bd40bd7984cc203
Andreas <aandreou25@gmail.com>
2025-04-21 19:18:20 +0100

    config.dart

--- splitleague_flutter/lib/helpers/config.dart
+++ splitleague_flutter/lib/helpers/config.dart
@@ -4,10 +4,12 @@ Contains global configuration settings like API base URL
 */
 
 class Config {
-  // Base URL for the API
+  // Local Host
   // static const String baseUrl = 'http://192.168.1.174:3000'; // Chippy
   // static const String baseUrl = 'http://192.168.1.88:3000'; // Home
   // static const String baseUrl = 'http://192.168.1.94:3000'; // 3 Cumberland
   // static const String baseUrl = 'http://10.249.1.230:43352'; // Grays
-  static const String baseUrl = 'http://77.168.13.150:3003'; // Test Server
+
+  // VPS Server
+  static const String baseUrl = 'http://77.68.13.150:3003'; // Test Server
 }
